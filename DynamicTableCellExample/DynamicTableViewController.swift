838e37f4bd5c36c5c360f7b315b4cc9302058eea
Lin He <he9lin@gmail.com>
2015-03-07 13:32:04 -0800

    Update DynamicTableViewController.swift

--- DynamicTableCellExample/DynamicTableViewController.swift
+++ DynamicTableCellExample/DynamicTableViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class DynamicTableViewController: UITableViewController {
   
-  var customCell: CustomTableViewCell?
-  
   let sentences = ["This is a pre-release version of the complete Xcode developer toolset for Mac, iPhone, iPad, and Apple Watch. This release requires OS X Yosemite.","This release of Xcode includes the Xcode IDE, LLVM compiler, Instruments, iOS Simulator, the latest OS X and iOS SDKs, and the new Swift programming language.","Post questions and share comments","Sign in with your Apple ID to download previous versions of Xcode or additional tools from the developer downloads site."]
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.tableView.estimatedRowHeight = 100.0
-    self.tableView.rowHeight = UITableViewAutomaticDimension
+    self.tableView.rowHeight = UITableViewAutomaticDimension // I thought this is the default
   }
   
   override func viewDidAppear(animated: Bool) {

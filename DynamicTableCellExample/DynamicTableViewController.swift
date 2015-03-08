787456c41012f0c29d69950f055c5602ce6d059c
Lin He <he9lin@gmail.com>
2015-03-08 13:55:54 -0700

    Re-arrange sentences to see affect

--- DynamicTableCellExample/DynamicTableViewController.swift
+++ DynamicTableCellExample/DynamicTableViewController.swift
@@ -24,13 +24,13 @@ func toJSONArray(object: JSON) -> JSONArray? {
 }
 
 let sentences = [
+  "Sign in with your Apple ID to download previous versions of Xcode or additional tools from the developer downloads site.",
+  "Post questions and share comments",
   "This is a pre-release version of the complete Xcode developer toolset for Mac, iPhone, iPad, and Apple Watch. This release requires OS X Yosemite.",
   "This release of Xcode includes the Xcode IDE, LLVM compiler, Instruments, iOS Simulator, the latest OS X and iOS SDKs, and the new Swift programming language.",
-  "Post questions and share comments",
-  "Sign in with your Apple ID to download previous versions of Xcode or additional tools from the developer downloads site.",
+  "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on. So keep looking. Don’t settle.",
   "When I was young, there was an amazing publication called The Whole Earth Catalog, which was one of the bibles of my generation. It was created by a fellow named Stewart Brand not far from here in Menlo Park",
   "Don't be trapped by dogma — which is living with the results of other people's thinking.",
-  "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on. So keep looking. Don’t settle.",
   "Stay hungry, stay foolish."
 ]
 
